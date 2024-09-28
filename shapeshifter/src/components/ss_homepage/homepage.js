4391a47934bcafafe73c3c2b2d6d38a923d4f059
PixlDix <ilianispps@gmail.com>
2024-09-28 01:45:13 -0400

    small change in homepage

--- shapeshifter/src/components/ss_homepage/homepage.js
+++ shapeshifter/src/components/ss_homepage/homepage.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import '../../App.css';
 import './homepage.css';
+import ParticleSys from '../particles/particle_sys';
 
 const Homepage = () => {
     document.title = 'ShapeShifter'
