1b3ed9004123bf734859c93bde94a7d0572e1b0b
ilian spasse <ilianispps@gmail.com>
2024-09-28 01:56:20 -0400

    added template for survey

---
+++ client/src/components/particles/particle_sys.js
@@ -0,0 +1,109 @@
+//This is just the particle system, can be imported to any page by including <ParticleSys /> in the rendering
+//don't forget to import ParticleSys from '../particles/particle_sys' at the top before you include it
+
+import { useCallback, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import './particle_sys.css'
+export default function Particle() {
+
+//particle and initialization stuff
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    console.log("init");
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+  };
+
+  //rendering
+  return (
+    //This is the entire particle system and its settings
+        <div className="background">
+        
+        <>
+        {init && (
+        <Particles
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            style={{
+            zIndex: 1,
+            }}
+            options={{
+            fpsLimit: 60,
+            interactivity: {
+                events: {
+                onClick: {
+                    enable: true,
+                    mode: "push",
+                },
+                onHover: {
+                    enable: true,
+                    mode: "repulse",
+                },
+                resize: true,
+                },
+                modes: {
+                push: {
+                    quantity: 4,
+                },
+                repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                },
+                },
+            },
+            particles: {
+                color: {
+                value: "#000",
+                },
+                links: {
+                color: "#000",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+                },
+                move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                    default: "bounce",
+                },
+                random: false,
+                speed: 1.2,
+                straight: false,
+                },
+                number: {
+                density: {
+                    enable: true,
+                    area: 800,
+                },
+                value: 160,
+                },
+                opacity: {
+                value: 0.5,
+                },
+                shape: {
+                type: "circle",
+                },
+                size: {
+                value: { min: 1, max: 5 },
+                },
+            },
+            detectRetina: true,
+            }}
+        />
+        )}
+        </>
+
+        </div>
+    
+  );
+
+}
---
+++ client/src/components/ss_landing/landing.js
@@ -0,0 +1,75 @@
+//ShapeShifter_Landing
+//This is the initial landing page - where you log in
+//if no account then you create one and it will lead to the survey
+import React from 'react';
+import '../../App.css'
+import './landing.css'
+import ParticleSys from '../particles/particle_sys'; 
+//import Homepage from '../ss_homepage/homepage';
+//import { NavLink, Link } from 'react-router-dom';
+
+//function call from log in
+//did it this way in case we need to add more logic to the click later
+function logIn(){
+  window.location = "/homepage";
+}
+
+//function call from create_account
+function createAcc(){
+  window.location = "/register";
+}
+
+
+const Landing = () => {
+  document.title = 'ShapeShifter'
+
+  //rendering
+  return (
+      <div>
+        {/*Log in Box*/}
+        <div className='authcontainer'>
+            <h1 className='authText'>Log In</h1>
+  
+            {/*Email Input*/}
+            <p className='authhelp'>Email:</p>
+            <div>
+                <input id='email' type='email' placeholder='Email' className='authbox'></input>
+            </div>
+
+            {/*Password Input*/}
+            <p className='authhelp'>Password:</p>
+            <div>
+                <input id='password' type='password' placeholder='Password' className='authbox'></input>
+            </div>
+
+            {/*Remember & Forgot Password*/}
+            <div>
+              <input type='checkbox' id = "rem_pass" value = "true"></input>
+              <label for="rem_pass">Remember Password?</label>
+
+              <a target='_blank' href='https://www.merriam-webster.com/dictionary/unlucky'>Forgot Password?</a>
+            </div>
+
+            {/*Log in & Create Account buttons*/}
+            <div className='button'>
+                <button className='authbutton' onClick={logIn}>Log In</button>
+            </div>
+            <div className='button'>
+                <button className='authbutton' onClick={createAcc}>Create Account</button>
+            </div>
+        </div>
+
+        {/*Particle System*/}
+        <ParticleSys />
+        
+      </div>
+    
+
+    
+
+    
+  );
+
+}
+
+export default Landing;
