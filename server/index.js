63805090dc51926da8aa39c5b795a1a94e3651e9
Rio Simpson <125085123+Briochee@users.noreply.github.com>
2024-10-06 21:33:33 -0400

    Update index.js
    reverted local host port

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = 4080;
+const PORT = 5000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
