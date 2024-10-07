477494df0a407ca00622214c216278160a9b9ff6
Rio Simpson <125085123+Briochee@users.noreply.github.com>
2024-10-06 21:32:42 -0400

    Update landing.js
    reverted local host port

--- client/src/components/ss_landing/landing.js
+++ client/src/components/ss_landing/landing.js
@@ -17,7 +17,7 @@ const Landing = () => {
       const password = document.getElementById('password').value;
 
       try {
-          const response = await fetch('http://localhost:4080/login', {
+          const response = await fetch('http://localhost:5000/login', {
               method: 'POST',
               headers: {
                   'Content-Type': 'application/x-www-form-urlencoded',
