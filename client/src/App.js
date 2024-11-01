850138ff72ddb21f3ff05ae6be0ce6a866f5ce30
hayimheron <102389144+hayimheron@users.noreply.github.com>
2024-10-31 22:52:31 -0400

    Update App.js pages

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import Survey from './components/ss_survey/survey';
 import WorkoutPlan from './components/ss_workoutplan/workoutplan';
 import Journal from './components/ss_journal/journal';
 import MealPlan from './components/ss_mealplan/mealplan';
+import Contact from './components/ss_contact/contact';
+import LiveWorkout from './components/ss_liveworkout/liveworkout';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -20,6 +23,9 @@ function App() {
         <Route path="/journal" element={<Journal />} />
         <Route path="/workoutplan" element={<WorkoutPlan />} />
         <Route path="/mealplan" element={<MealPlan />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/liveworkout" element={<LiveWorkout />} />
+
       </Routes>
     </BrowserRouter>
   );
