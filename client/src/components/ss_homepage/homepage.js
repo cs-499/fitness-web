0982bc88a2cc6928f7016719162fae10b46c47dd
hayimheron <hayimheron@gmail.com>
2024-10-31 23:39:14 -0400

    add buttons to homepage

--- client/src/components/ss_homepage/homepage.js
+++ client/src/components/ss_homepage/homepage.js
@@ -1,71 +1,100 @@
-//This page will be the Homepage where you see your workouts
+// This page will be the Homepage where you see your workouts
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.css';
 import './homepage.css';
 import WorkoutImage from '../images/chest.png';
+import WorkoutImage2 from '../images/legs.avif';
+import WorkoutImage3 from '../images/biceps.webp';
+import WorkoutImage4 from '../images/abs.jpg';
 import NavBar from "../navbar/nav_bar";
 import ParticleSys from '../particles/particle_sys';
 
 const Homepage = () => {
-    document.title = 'ShapeShifter'
-    return(
+    useEffect(() => {
+        document.title = 'ShapeShifter';
+    }, []);
+
+    const ImageSlider = () => {
+        const images = [WorkoutImage, WorkoutImage2, WorkoutImage3, WorkoutImage4];
+        const workoutNames = ["Chest", "Legs", "Biceps", "Abs"];
+        const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+        const goToPrevious = () => {
+            setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+        };
+
+        const goToNext = () => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        };
+
+        return (
+            <div className='today_workout'>
+                <img className='body_highlight' src={images[currentImageIndex]} alt="Workout Image" />
+                <h1 className='actual_workout'>{workoutNames[currentImageIndex]}</h1>
+                <button className="left-arrow" onClick={goToPrevious}>❮</button>
+                <button className="right-arrow" onClick={goToNext}>❯</button>
+            </div>
+        );
+    };
+
+    return (
         <div className='all'>
             <NavBar />
             <div className='Workoutout_Meals'>
                 <div className='todayW'>
-                <h1 className='category_text'>Today's Workout</h1>
-                    <div className='today_workout'>
-                    
-                        <img className='body_highlight' src={WorkoutImage} />
-                        <h1 className='actual_workout'>Chest</h1>
-                        
-                    </div>
-                    <button class="done-button">Workout Complete</button>
+                    <h1 className='category_text'>Today's Workout</h1>
+                    <ImageSlider />
+
+                    <button 
+                        className="start-button" 
+                        onClick={() => window.location.href = 'http://localhost:3000/liveworkout'}>
+                        Start Workout
+                    </button>
+                    <button className="done-button">Workout Complete</button>
                 </div>
 
-                <div class="meals-section">
+                <div className="meals-section">
                     <h1 className='meal_category'>Planned Meals</h1>
 
-                    <div class="meal-item">
-                        <div class="meal-image">
-                            <div class="image-placeholder"></div>
+                    <div className="meal-item">
+                        <div className="meal-image">
+                            <div className="image-placeholder"></div>
                         </div>
-                        <div class="meal-info">
+                        <div className="meal-info">
                             <h3>Meal name</h3>
                             <p>Other info</p>
                         </div>
-                        <button class="log-button">Log</button>
+                        <button className="log-button">Log</button>
                     </div>
 
-                    <div class="meal-item">
-                        <div class="meal-image">
-                            <div class="image-placeholder"></div>
+                    <div className="meal-item">
+                        <div className="meal-image">
+                            <div className="image-placeholder"></div>
                         </div>
-                        <div class="meal-info">
+                        <div className="meal-info">
                             <h3>Meal name</h3>
                             <p>Other info</p>
                         </div>
-                        <button class="log-button">Log</button>
+                        <button className="log-button">Log</button>
                     </div>
 
-                    <div class="meal-item">
-                        <div class="meal-image">
-                            <div class="image-placeholder"></div>
+                    <div className="meal-item">
+                        <div className="meal-image">
+                            <div className="image-placeholder"></div>
                         </div>
-                        <div class="meal-info">
+                        <div className="meal-info">
                             <h3>Meal name</h3>
                             <p>Other info</p>
                         </div>
-                        <button class="log-button">Log</button>
+                        <button className="log-button">Log</button>
                     </div>
                 </div>
             </div>
-            
 
             <ParticleSys />
         </div>
-    )
+    );
 }
 
 export default Homepage;
