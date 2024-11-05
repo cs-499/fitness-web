d0fdbdc864dc264914dfa3ef58f00070d7b02c29
Rio Simpson <rio.simpsonnyc@gmail.com>
2024-11-04 19:26:07 -0500

    added stylesheet for mealplan page and redirect to generator

--- client/src/components/ss_mealplan/mealplan.js
+++ client/src/components/ss_mealplan/mealplan.js
@@ -1,18 +1,23 @@
 //This page will be the mealplan page, where you will see the users particular mealplan
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../App.css';
 import NavBar from "../navbar/nav_bar"
 import ParticleSys from '../particles/particle_sys';
 
 const mealPlan = () => {
     document.title = 'ShapeShifter'
-    return(
+    const handleNavigate = () => {
+        window.location.href = '/mealgenerator';
+    };
+
+    return (
         <>
             <NavBar />
-            Meal plan!
+            <h1>Meal Plan</h1>
+            <button onClick={handleNavigate}>Go to Meal Generator</button>
         </>
-    )
+    );
 }
 
 export default mealPlan;
