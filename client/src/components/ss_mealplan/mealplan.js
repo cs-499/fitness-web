d6730f57ba95f4b6b578f2c493daa5581fb22f53
hayimheron <102389144+hayimheron@users.noreply.github.com>
2024-11-20 12:41:05 -0500

    Update mealplan.js

--- client/src/components/ss_mealplan/mealplan.js
+++ client/src/components/ss_mealplan/mealplan.js
@@ -1,23 +1,46 @@
-//This page will be the mealplan page, where you will see the users particular mealplan
-
-import React, { useState, useEffect } from 'react';
+=import React, { useState } from 'react';
 import '../../App.css';
-import NavBar from "../navbar/nav_bar"
-import ParticleSys from '../particles/particle_sys';
+import NavBar from "../navbar/nav_bar";
 
-const mealPlan = () => {
-    document.title = 'ShapeShifter'
+const MealPlan = () => {
+    document.title = 'ShapeShifter';
+
+    const [workoutGoal, setWorkoutGoal] = useState('');
+    const [dietGoal, setDietGoal] = useState('');
     const handleNavigate = () => {
-        window.location.href = '/mealgenerator';
+        // Pass goals as query parameters
+        const params = new URLSearchParams({ workoutGoal, dietGoal }).toString();
+        window.location.href = `/mealgenerator?${params}`;
     };
 
     return (
         <>
             <NavBar />
             <h1>Meal Plan</h1>
-            <button onClick={handleNavigate}>Go to Meal Generator</button>
+            <div className="goal-inputs">
+                <h2>Set Your Goals</h2>
+                <div>
+                    <label>Workout Goal:</label>
+                    <input
+                        type="text"
+                        value={workoutGoal}
+                        onChange={(e) => setWorkoutGoal(e.target.value)}
+                        placeholder="E.g., Gain muscle, lose weight"
+                    />
+                </div>
+                <div>
+                    <label>Diet Goal:</label>
+                    <input
+                        type="text"
+                        value={dietGoal}
+                        onChange={(e) => setDietGoal(e.target.value)}
+                        placeholder="E.g., High protein, keto"
+                    />
+                </div>
+                <button onClick={handleNavigate}>Generate Meal Plan</button>
+            </div>
         </>
     );
-}
+};
 
-export default mealPlan;
+export default MealPlan;
