a1046e9a03b41e75c7bcff221e2aa76051ffd7c0
Rio Simpson <rio.simpsonnyc@gmail.com>
2024-12-04 16:24:54 +0100

    implemented user data retrieval for filter use

--- client/src/components/ss_mealplan/mealplan.js
+++ client/src/components/ss_mealplan/mealplan.js
@@ -1,15 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../App.css';
 import './mealplan.css';
 import NavBar from "../navbar/nav_bar";
 import { useNavigate } from 'react-router-dom';
 
 const MealPlan = () => {
-    document.title = 'ShapeShifter';
-
     const [workoutGoal, setWorkoutGoal] = useState('');
     const [dietGoal, setDietGoal] = useState('');
     const navigate = useNavigate();
+    const [surveyData, setSurveyData] = useState([]);
+
+    useEffect(() => {
+        document.title = 'Meal Plan Homepage';
+        fetchSurveyData().then(data => {
+            if (data && data.length > 0) {
+                const formattedData = data.flatMap(survey => survey.answers.map(entry => ({
+                    question: entry[0], // question
+                    values: entry[1].value, // value and questiontarget
+                    // questionTarget: entry[1].questionTarget
+                })));
+                setSurveyData(formattedData);
+                // console.log("Formatted Survey Data:", formattedData);
+            }
+        }).catch(error => {
+            console.error("Failed to fetch data on mount:", error);
+        });
+    }, []);
+
+    async function fetchSurveyData() {
+        const userId = localStorage.getItem('userId');
+        const token = localStorage.getItem('token');
+    
+        try {
+            const apiURL = `${process.env.REACT_APP_API_HOST}/survey/${userId}?questionTarget=meal`;
+            const response = await fetch(apiURL, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+    
+            if (!response.ok) {
+                console.error('No survey data found for this user');
+                return []; // Return an empty array to handle failures gracefully
+            }
+    
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error fetching survey data:', error);
+            return [];
+        }
+    }
 
     const handleNavigate = () => {
         if (!workoutGoal.trim() && !dietGoal.trim()) {
