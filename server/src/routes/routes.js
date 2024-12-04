fa044e0c28ea8ba0b181881142eff5a8a2f3b848
Rio Simpson <rio.simpsonnyc@gmail.com>
2024-12-04 13:55:53 +0100

    additional routes for CRUD calls

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { login, register } from '../controllers/logging.js';
-import { submitSurvey, getSurveyFromUser } from '../controllers/survey.js';
+import { submitSurvey, getSurveyFromUser, deleteSurvey, checkSurveyCompletion } from '../controllers/survey.js';
 import verifyToken from '../middleware/auth.js';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.post('/login', login);
 router.post('/register', register);
 router.post('/submit', verifyToken, submitSurvey);
 router.get('/:userId', verifyToken, getSurveyFromUser);
+router.delete('/delete-survey/:userId', verifyToken, deleteSurvey);
+router.get('/check-completion/:userId', verifyToken, checkSurveyCompletion);
 
 export default router;
